f51d7e347a405d4b35fbbc9c8855e59b3e74a458
Thomas Anquetil <36239513+ThomasAqtl@users.noreply.github.com>
2020-12-22 20:00:08 +0100

    Acceleration progressive et début de mur derrière la moto

--- Assets/Scripts/ControllerManager.cs
+++ Assets/Scripts/ControllerManager.cs
@@ -7,6 +7,8 @@ public class ControllerManager : MonoBehaviour
     public KeyCode forwardKey, leftKey, rightKey;
     public float tilt;
     private float _fwd, _lateralDirection, _right, _left;
+    public const float _fwdmin = 0.3f;
+    public const float _fwdmax = 1.0f;
     public float Fwd{   
         get { return _fwd;}
     }
@@ -15,19 +17,19 @@ public class ControllerManager : MonoBehaviour
     }
 
     public void Start(){
-        _fwd = 0;
+        _fwd = _fwdmin;
         _right = 0;
         _left = 0;
     }
 
     public void FixedUpdate(){
         // _fwd and _lateralDirection kind of simulates default horizontal and vertical axis values
-        _fwd += Input.GetKey(forwardKey) ? (_fwd < 1 ? 0.1f : 0) : (_fwd > 0 ? (_fwd < 0.01f ? -_fwd : -0.1f) : 0);
+        _fwd += Input.GetKey(forwardKey) ? (_fwd < 1 ? 0.01f : 0) : (_fwd > _fwdmin ? (_fwd < 0.01f ? -_fwd : -0.01f) : 0);
         
         // moto can't turn if not moving forward
-        _right += Input.GetKey(rightKey) & _fwd != 0 ? (_right < 1 ? 0.1f : 0) : (_right > 0 ? (_right < 0.1f ? -_right : -0.1f) : -_right);
+        _right += Input.GetKey(rightKey) ? (_right < 1 ? 0.1f : 0) : (_right > 0 ? (_right < 0.1f ? -_right : -0.1f) : -_right);
 
-        _left += Input.GetKey(leftKey) & _fwd != 0  ? (_left > -1 ? -0.1f : 0) : (_left < 0 ? (_left > 0.1f ? -_left : 0.1f) : -_left);
+        _left += Input.GetKey(leftKey)  ? (_left > -1 ? -0.1f : 0) : (_left < 0 ? (_left > 0.1f ? -_left : 0.1f) : -_left);
 
         _lateralDirection = _left + _right;
     }
--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Movements : MonoBehaviour
 {
+    public float tilt;
     public float speed;
     public float sensibility;
     private ControllerManager _controller;
@@ -16,7 +17,18 @@ public class Movements : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+         
         transform.Translate(Vector3.right * _controller.Fwd * speed * Time.deltaTime, Space.Self);
         transform.Rotate(Vector3.up * _controller.LateralDirection * sensibility * Time.deltaTime, Space.World);
+    }
+
+
+    void FixedUpdate()
+    { /*
+        float tiltAroundX = _controller.LateralDirection * -tilt;
+
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, 0);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime*2f); */
     }
 }
